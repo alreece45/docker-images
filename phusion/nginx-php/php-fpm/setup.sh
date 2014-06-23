3d106b65b47e39ed166457f314bb4e8a9196279d
Alexander Reece <github@reece45.com>
2014-06-23 15:33:38 -0400

    Correct PHP-FPM socket permissions

--- phusion/nginx-php/php-fpm/setup.sh
+++ phusion/nginx-php/php-fpm/setup.sh
@@ -12,3 +12,6 @@ mkdir /etc/service/php-fpm
 cp -a /build/php-fpm/runit.sh /etc/service/php-fpm/run
 chmod +x /etc/service/php-fpm/run
 php5enmod mcrypt
+
+# Ensure the mode is correct on the unix socket
+sed -i 's#;listen.mode = 0660#listen.mode = 0666#g' /etc/php5/fpm/pool.d/www.conf
