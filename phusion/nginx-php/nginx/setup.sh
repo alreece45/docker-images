64ba05fdb0de361c8868b0b89c95c107df029fc6
Alexander Reece <github@reece45.com>
2014-08-11 23:29:58 -0400

    Update spacing in nginx setup script

--- phusion/nginx-php/nginx/setup.sh
+++ phusion/nginx-php/nginx/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "daemon off;" >> /etc/nginx/nginx.conf
 cp -a /build/nginx/site-default /etc/nginx/sites-available/default
+
 # Configure nginx (docroot and front controller) on startup
 cp -a /build/nginx/init.sh /etc/my_init.d/00_configure_nginx.sh
 chmod +x /etc/my_init.d/00_configure_nginx.sh
