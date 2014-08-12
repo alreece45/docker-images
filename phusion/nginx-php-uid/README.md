c56f7e99080e6d36e1ce0d8ac3845d7a6ac8d8cb
Alexander Reece <github@reece45.com>
2014-08-11 22:40:52 -0400

    Add the "UID Sync" as an option to the nginx-php image

--- phusion/nginx-php-uid/README.md
+++
@@ -1,1 +0,0 @@
-nginx-php build with a tweak to make the www-data uid match the document root.
--- phusion/nginx-php/nginx/init.sh
+++ phusion/nginx-php/nginx/init.sh
@@ -16,3 +16,22 @@ then
     sed -i "s#try_files \$uri \$uri/ /index.html /index.php?\$query_string;#try_files \$uri \$uri/ /index.html /${FRONT_CONTROLLER}?;#" /etc/nginx/sites-enabled/default
 fi
 
+if [ $SYNC_UID -eq 1 ]
+then
+    uid=`stat -c '%u' $DOCROOT`
+    gid=`stat -c '%g' $DOCROOT`
+    
+    echo "Updating www-data ids to ($uid:$gid)"
+
+    if [ ! $uid -eq 0 ]
+    then
+        sed -i "s#^www-data:x:.*:.*:#www-data:x:$uid:$gid:#" /etc/passwd
+    fi
+
+    if [ ! $gid -eq 0 ]
+    then
+        sed -i "s#^www-data:x:.*:#www-data:x:$gid:#" /etc/group
+    fi
+
+    chown www-data /var/log/nginx
+fi
