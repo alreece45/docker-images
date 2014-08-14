74d9c663689f4861a5e316648a51f32a4451cbec
Alexander Reece <github@reece45.com>
2014-08-13 21:02:27 -0400

    Create a mysql-base image

---
+++ phusion/mysql-base/packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+echo force-unsafe-io > /etc/dpkg/dpkg.cfg.d/02apt-speedup
+apt-get update
+apt-get --no-install-recommends -y install mysql-client
+
+# Cleanup
+apt-get clean
+rm -f /etc/dpkg/dpkg.cfg.d/02apt-speedup
+find /tmp /var/tmp -mindepth 2 -delete
+find /build -delete
