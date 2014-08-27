bd43cc674fd7b428564a93f1e0d5308cb797e361
Alexander Reece <github@reece45.com>
2014-08-27 00:12:26 -0400

    [debian-php-fpm:wheezy-debug] New Image

---
+++ php-fpm/debian_wheezy-debug/packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+echo force-unsafe-io > /etc/dpkg/dpkg.cfg.d/02apt-speedup
+apt-get update
+apt-get --no-install-recommends -y install php5-fpm php5-xdebug
+php5enmod xdebug
+
+# Cleanup
+apt-get clean
+rm -f /etc/dpkg/dpkg.cfg.d/02apt-speedup
+find /var/lib/apt/lists -mindepth 1 -delete -print
+find /tmp /var/tmp -mindepth 2 -delete -print
+find /build -delete
