d3ef304f281bb4f2a6ba1449143ea06d6c3f3e1d
Alexander Reece <github@reece45.com>
2014-08-28 16:20:38 -0400

    [ubuntu-mysql:trusty] Log errors to console

--- mysql/mysql/ubuntu_trusty/service/setup.sh
+++ mysql/mysql/ubuntu_trusty/service/setup.sh
@@ -3,6 +3,9 @@
 # Listen on all interfaces
 sed -i "s#^bind-address\s*=\s*.*#bind-address = 0.0.0.0#" /etc/mysql/my.cnf
 
+# Log to stdout (remove logfile entry)
+sed -i 's#^log_error = /var/log/mysql/error.log#console#' /etc/mysql/my.cnf
+
 # Configure mysql to start as a service
 mkdir /opt/docker-mysql
 cp -a /build/mysql/service.sh /opt/init-mysql.sh
