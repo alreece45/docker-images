08aef87d46310506473bb5a897441bd161e9d113
Alexander Reece <github@reece45.com>
2014-08-29 23:50:32 -0400

    [ubuntu-icinga2:trusty] Add option to configure node name

--- icinga/icinga2/ubuntu_trusty/service.sh
+++ icinga/icinga2/ubuntu_trusty/service.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+sed_escape() {
+	echo "$*" | sed 's/"/\\\\"/g' | sed 's/#/\\#/g'
+}
+
 # Run a few checks before staring in the foregronud
 # The init script runs these checks when "start" is used
 # But does not run them when when "foreground" is used
@@ -22,5 +26,12 @@ chmod 2710 /var/run/icinga2/cmd
 test -d '/var/log/icinga2' || mkdir /var/log/icinga2
 chown "$DAEMON_USER":adm /var/log/icinga2
 chmod 0761 /var/log/icinga2
+
+# Provide an option to change the node name (the random ID may not be ideal)
+if [ -n "$NODE_NAME" ]
+then
+	sed_node_name=`sed_escape "$NODE_NAME"`
+	sed -r -i 's#^(//)?const NodeName = ".*"#const NodeName = "'"$sed_node_name"'"#g' /etc/icinga2/constants.conf
+fi
 
 exec /etc/init.d/icinga2 foreground
