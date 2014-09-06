0c633d6bf97834029dd4bfa59c05eeb48491f0fd
Alexander Reece <github@reece45.com>
2014-09-05 23:15:19 -0400

    [phusion-mysql:trusty] Rename service directory (to setup)

--- mysql/mysql/phusion_trusty/setup/setup.sh
+++ mysql/mysql/phusion_trusty/setup/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-/build/mysql/ubuntu-setup.sh
+cd `dirname $0`
+. ./ubuntu-setup.sh
 
 # Configure mysql to start as a service
 mkdir /etc/service/mysql
-cp -a /build/mysql/service.sh /etc/service/mysql/run
+cp -a ./service.sh /etc/service/mysql/run
 chmod +x /etc/service/mysql/run
 
 cat /dev/null > /var/log/mysql/error.log
