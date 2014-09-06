0358c0c375f9981f3e624507dbf37a28304f7ca8
Alexander Reece <github@reece45.com>
2014-09-05 22:22:40 -0400

    [ubuntu-icinga2:trusty] Use the common/debian preinstall and postinstall scripts

---
+++ icinga2/ubuntu_trusty/installer/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+# Install icinga2 PPA
+command -v apt-add-repository
+
+if [ $? -eq 0 ]
+then
+	apt-add-repository ppa:formorer/icinga
+else
+	echo "deb http://ppa.launchpad.net/formorer/icinga/ubuntu trusty main" > /etc/apt/sources.list.d/formorer-icinga-trusty.list
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36862847
+fi
+
+# Update surces and install packages
+. ./common-debian-packages/preinstall.sh
+apt-get -y install icinga2
+. ./common-debian-packages/postinstall.sh
