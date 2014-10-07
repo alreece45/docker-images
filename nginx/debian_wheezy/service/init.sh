2743993acf0dbb1eefd526e541a5e8996631ac69
Alexander Reece <github@reece45.com>
2014-10-07 00:18:33 -0400

    [debian-nginx:wheezy] Reuse common-app uid-gid-config

--- nginx/debian_wheezy/service/init.sh
+++ nginx/debian_wheezy/service/init.sh
@@ -1,57 +1,22 @@
 #!/bin/sh -x
 
 # Use the TZ environment variable, otherwise use UTC
-NGINX_DOCROOT="/usr/share/nginx/html"
+NGINX_DOCROOT=""
 if [ -n "${DOCROOT}" ]
 then
-    NGINX_DOCROOT="$DOCROOT"
+    APP_ROOT="$DOCROOT"
+fi
+
+if [ -z "$APP_ROOT"]
+then
+    APP_ROOT="/usr/share/nginx/html"
 fi
 
 # Update the document root configuration, create the document root if needed
-sed -i "s#root .*;#root $NGINX_DOCROOT;#" /etc/nginx/sites-enabled/default
-mkdir -p $NGINX_DOCROOT
+sed -i "s#root .*;#root $APP_ROOT;#" /etc/nginx/sites-enabled/default
+mkdir -p $APP_ROOT
 
-# Check to see if we should get the nginx uid/gid from the document root
-if [ "$SYNC_UID" = "1" ]
-then
-    APP_UID_GID_FROM="$NGINX_DOCROOT"
-fi
-if [ "$APP_UID_FROM_DOCROOT" = "1" ]
-then
-    APP_UID_FROM="$NGINX_DOCROOT"
-fi
-if [ "$APP_GID_FROM_DOCROOT" = "1" ]
-then
-    APP_GID_FROM="$NGINX_DOCROOT"
-fi
-if [ "$APP_UID_GID_FROM_DOCROOT" = "1" ]
-then
-    APP_UID_GID_FROM="$NGINX_DOCROOT"
-fi
-
-# Check to see if there's a path to get uid and gid from
-if [ -n "$APP_UID_FROM" ]
-then
-    if [ -n "$APP_UID" ]
-    then
-        APP_UID=`stat -c '%u' $APP_UID_FROM`
-    fi
-fi
-if [ -n "$APP_GID_FROM" ]
-then
-    if [ -n "$APP_GID" ]
-    then
-        APP_GID=`stat -c '%g' $APP_GID_FROM`
-    fi
-fi
-if [ -n "$APP_UID_GID_FROM" ]
-then
-    if [ -n "$APP_GID" ]
-    then
-        APP_UID=`stat -c '%u' $APP_UID_GID_FROM`
-        APP_GID=`stat -c '%g' $APP_UID_GID_FROM`
-    fi
-fi
+. ./common-debian-app/uid-gid-config.sh
 
 # Change the uid of the www-data user, if specified
 if [ -n "$APP_UID" ]
@@ -60,12 +25,12 @@ then
     then
         APP_GID=`getent group www-data`
     fi
+
     echo "Updating www-data ids to ($APP_UID:$APP_GID)"
 
     sed -i "s#^www-data:x:.*:.*:#www-data:x:$APP_UID:$APP_GID:#" /etc/passwd
     sed -i "s#^www-data:x:.*:#www-data:x:$APP_GID:#" /etc/group
     chown www-data /var/log/nginx
 fi
-
 
 exec nginx
