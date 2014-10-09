3ee9b67255a855abf5ec1c0dd7f394261b5b1f86
Alexander Reece <github@reece45.com>
2014-10-09 11:48:29 -0400

    [debian-php-fpm:wheezy] Move the php.ini configuration update to its own script

--- php-fpm/debian_wheezy/service/init.sh
+++ php-fpm/debian_wheezy/service/init.sh
@@ -10,15 +10,9 @@ fi
 
 sed -i "s#^listen = .*#listen = $LISTEN#g" /etc/php5/fpm/pool.d/www.conf
 
-# Use the TZ environment variable, otherwise use UTC
-PHP_TIMEZONE="UTC"
-if [ -n "${TZ}" ]
-then
-    PHP_TIMEZONE="$TZ"
-fi
-
 find /etc/php5 -name php.ini -print0 | xargs -0 sed -i "s#;date.timezone =.*#date.timezone = $PHP_TIMEZONE#"
 
+. ./configure-php-ini.sh
 . ./configure-fpm-environment.sh
 
 exec php5-fpm -c /etc/php5/fpm
